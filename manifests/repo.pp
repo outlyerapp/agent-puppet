9f9e0d485f7840d0d38c79a34fc8dfae0d91d02e
Tom Ashley <tom.ashley@dataloop.io>
2015-03-26 16:46:21 +0000

    align with ubuntu 12.04

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -5,17 +5,17 @@ class dataloop_agent::repo() {
    'CloudLinux', 'PSBM', 'OracleLinux', 'OVS', 'OEL', 'Amazon', 'XenServer': {
   
       yumrepo { 'dataloop':
-        baseurl => "https://download.dataloop.io/packages/stable/rpm/$architecture",
-        descr => 'Dataloop Repository',
-        enabled => 1,
+        baseurl  => "https://download.dataloop.io/packages/stable/rpm/$architecture",
+        descr    => 'Dataloop Repository',
+        enabled  => 1,
         # GPG is disabled until packages are GPG signed
         gpgcheck => 0,
       }
 
-      exec { "clean_yum_metadata":
-        command => "/usr/bin/yum clean metadata",
+      exec { 'clean_yum_metadata':
+        command     => '/usr/bin/yum clean metadata',
         refreshonly => true,
-        require => Yumrepo['dataloop'],
+        require     => Yumrepo['dataloop'],
       }
 
     }
@@ -25,10 +25,9 @@ class dataloop_agent::repo() {
 
       $apt_key_url = 'https://download.dataloop.io/pubkey.gpg'
 
-      exec { "apt-key dataloop ":
-        name    => 'dataloop',
-        command => "/usr/bin/wget -q $apt_key_url -O -|/usr/bin/apt-key add -",
-        unless  => "/usr/bin/apt-key list|/bin/grep -c $name",
+      exec { 'apt-key dataloop':
+        command => "/usr/bin/wget -q ${apt_key_url} -O -|/usr/bin/apt-key add -",
+        unless  => '/usr/bin/apt-key list|/bin/grep -c dataloop',
       }
   
       apt::source { 'dataloop':
